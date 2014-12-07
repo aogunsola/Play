635ce08bed7725bd0d97652c5dc21e6e6ac4a6cb
Ayoola Ogunsola <aogunsola@asicentral.com>
2014-12-07 14:22:30 -0500

    added pictures

--- jqm/MoveASauras/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ayoola/Documents/GitHub/Play/jqm/MoveASauras/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/ayoola/Documents/GitHub/Play/jqm/MoveASauras/platforms/android/CordovaLib/AndroidManifest.xml \
--- jqm/MoveASauras/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ayoola/documents/github/play/jqm/moveasauras/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/ayoola/documents/github/play/jqm/moveasauras/platforms/android/AndroidManifest.xml \
